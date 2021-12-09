854308446a26b12910dcbbf1b702182b7f82b2d9
PonomareVlad <Git@PonomareVlad.ru>
2021-12-09 16:40:01 +0500

    Echo answer

--- bot.mjs
+++ bot.mjs
@@ -1,13 +1,27 @@
 import TeleBot from "telebot"
 
-const bot = new TeleBot(process.env.TELEGRAM_BOT_TOKEN), intro = [`Приветствую Вас, я бортовой компьютер Первого Новотрубного Телепорта к Звездам. 
+const intro = [`Приветствую Вас, я бортовой компьютер Первого Новотрубного Телепорта к Звездам. 
 Совсем скоро, 23 декабря, мы совершим стыковку к ТМКс (Трубной металлургической космической станции). 
-Здесь я поделюсь информацией и указаниями по подготовке к телепортации и стыковке 🚀🚀🚀`, `Следите за новостями и готовьтесь к полету вместе со мной!`]
+Здесь я поделюсь информацией и указаниями по подготовке к телепортации и стыковке 🚀🚀🚀`, `Следите за новостями и готовьтесь к полету вместе со мной!`],
+    echo = 'Бортовой компьютер ПНТЗ взял ваш запрос в обработку'
 
-bot.on('/start', async msg => await intro.forEachAsync(msg.reply.text))
+export class Bot extends TeleBot {
+    constructor(...args) {
+        super(...args)
+        this.on('text', msg => this.constructor.isCommand(msg.text) ? null : msg.reply.text(echo));
+        this.on('/start', async msg => await intro.forEachAsync(msg.reply.text))
+        this.mod('message', data => {
+            if (this.constructor.isCommand(data.message.text))
+                data.message.payload = data.message.text.split(' ').splice(1).join(' ')
+            return data;
+        });
+    }
+
+    static isCommand = text => text[0] === '/'
+}
 
 Array.prototype.forEachAsync = async function (fn) {
     for (let t of this) await fn(t)
 }
 
-export default bot
+export default new Bot(process.env.TELEGRAM_BOT_TOKEN)
