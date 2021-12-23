d2bb9bfca7f0f88603ece10dcdb8015ed36dbef1
PonomareVlad <Git@PonomareVlad.ru>
2021-12-23 14:50:01 +0500

    New broadcast

--- api/telegram.mjs
+++ api/telegram.mjs
@@ -3,5 +3,5 @@ import mongo from '../db.mjs'
 
 const log = mongo.db("PNTZ").collection("Log")
 
-export default async ({body}, {json}) => console.debug(body) || await log.insertOne(body) &&
+export default async ({body}, {json}) => console.debug(body.message) || await log.insertOne(body) &&
     await json(body && body.update_id ? await bot.receiveUpdates([body]) : {status: false})
